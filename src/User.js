165f5f5a0379c365a774bdf42bfeee10c53b87f3
Jeff Schuman <jschuman@solutionstreet.com>
2021-10-06 13:16:57 -0400

    refactor User back to a functional component

--- src/User.js
+++ src/User.js
@@ -8,35 +8,32 @@ import Button from 'react-bootstrap/Button';
 
 import { requestUser } from './actions';
 
+function User(props) {
+  let user = props.user;
 
-class User extends React.Component {
-
-  onClickSelect() {
-    this.props.requestUser(this.props.user.id);
+  const onClickSelect = () => {
+    props.requestUser(props.user.id);
   }
 
-  render() {
-    let user = this.props.user;
-    return ( 
-      <Row className="justify-content-md-center">
-        <Col lg={6}>
-          <Card>
-            <Card.Body>
-              <h1>{user.name}</h1>
-              <p>{user.email}</p>
-              <p>{user.street}</p>
-              <p>{user.address.city}, {user.address.zipcode}</p>
-              <p><a href={`http://${user.website}`}>My Website</a></p>
-              <Button variant='success' onClick={this.onClickSelect.bind(this)}>Select</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    );
-  }
+  return ( 
+    <Row className="justify-content-md-center">
+      <Col lg={6}>
+        <Card>
+          <Card.Body>
+            <h1>{user.name}</h1>
+            <p>{user.email}</p>
+            <p>{user.street}</p>
+            <p>{user.address.city}, {user.address.zipcode}</p>
+            <p><a href={`http://${user.website}`}>My Website</a></p>
+            <Button variant='success' onClick={onClickSelect}>Select</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  )
 }
 
-const mapDispatchToProps = dispatch => 
+const mapDispatchToProps = (dispatch) => 
   bindActionCreators({ requestUser }, dispatch);
 
 export default connect(null, mapDispatchToProps)(User);
